416029d19db7442ddd2e3ede52bc8ea2fda0fcca
Ru960413 <wendyhu0413@gmail.com>
2023-05-24 16:04:10 +0800

    queryselecting btns and create func (unfinished)

--- cocktail.js
+++ cocktail.js
@@ -2,20 +2,32 @@
 const btnCocktail = document.querySelector(".btn-cocktail");
 const cocktailsContainer = document.querySelector(".cocktails");
 const btnNonAlcoholic = document.querySelector(".btn-non-alcoholic");
-const btnAlcoholic = document.querySelector(".btn-alcoholic");
+const searchBtn = document.querySelector(".search_btn");
+const bookmarked = document.querySelector(".bookmarked");
+const bookmarkBtn = document.querySelector(".bookmark_btn");
 
 // TODOs:
 // 1. allow users to search cocktail by name then render search result as list
 // 2. allow users to add cocktail to bookmark
 // 3. allow users to view bookmarked cocktails
 // 4. add pagination
+// 5. structure js files
 
 // Extras:
 // 1. add servings calculator
 
+let bookMarked = [];
 const randomNum = function (length) {
   return Math.floor(Math.random() * (length + 1));
 };
+const renderSearchResult = function (name) {};
+
+// Need to get the data to add it to bookmark, but how?
+const addAsBookmark = function (data) {
+  bookMarked.push(data);
+  console.log(bookMarked);
+};
+
 const renderCocktail = function (data) {
   while (cocktailsContainer.firstChild) {
     cocktailsContainer.removeChild(cocktailsContainer.firstChild);
@@ -24,7 +36,11 @@ const renderCocktail = function (data) {
   <article class="cocktail">
     <img class="cocktail__img" src="${data.strDrinkThumb}" />
     <div class="cocktail__data">
-      <h3 class="cocktail__name">${data.strDrink}</h3>
+      <div class="cocktail_illustration">
+        <h3 class="cocktail__name">${data.strDrink}</h3>
+        <div class="bookmark_btn"><span class="material-symbols-outlined">
+        bookmark</span>Bookmark</div>
+      </div>
       <h4 class="cocktail__type">${data.strAlcoholic}</h4>
       <p class="cocktail__row"><span>Category</span>${data.strCategory}</p>
       <p class="cocktail__row"><span>Glass</span>${data.strGlass}</p>
@@ -53,17 +69,6 @@ const renderCocktail = function (data) {
 `;
   cocktailsContainer.insertAdjacentHTML("beforeend", html);
 };
-
-// const getRandomCocktail = function () {
-//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-//     .then((res) => res.json())
-//     .then(function (data) {
-//       data = data.drinks[0];
-//       console.log(data);
-//       renderCocktail(data);
-//     })
-//     .finally((cocktailsContainer.style.opacity = 1));
-// };
 
 // ISSUE: cannot generate random drink SOLVED
 // Can only get the id of drink, need to use id to get other details (chaining promises)
@@ -115,4 +120,7 @@ const getAlcoholic = function () {
 
 btnCocktail.addEventListener("click", getAlcoholic);
 btnNonAlcoholic.addEventListener("click", getNonAlcoholic);
-//btnAlcoholic.addEventListener("click", getAlcoholic);
+
+if (bookmarkBtn) {
+  bookmarkBtn.addEventListener("click", addAsBookmark);
+}
